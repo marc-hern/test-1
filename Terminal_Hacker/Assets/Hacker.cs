d33e92fa9669b126299da212db1effb21ded0077
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-08-27 23:54:41 -0700

    Main Menu for initial screen

---
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		ShowMainMenu("Logged in...");
+	}
+
+	void ShowMainMenu(string logIn) {
+		Terminal.ClearScreen();
+
+		Terminal.WriteLine(logIn);
+		Terminal.WriteLine("Hack locations: ");
+		Terminal.WriteLine("1.  Local");
+		Terminal.WriteLine("1.  Startup");
+		Terminal.WriteLine("1.  Google");
+		Terminal.WriteLine("Enter selection:");
+	}
+
+	void OnUserInput(string input){
+		print(input);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
