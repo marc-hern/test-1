4d7536bf8f070de4da57e25eab8523ab116cd639
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-08-29 23:21:44 -0700

    Password lists and checks

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -11,7 +11,25 @@ public class Hacker : MonoBehaviour {
 		Password,
 		Win
 	};
-	Screen currentScreen = Screen.MainMenu;
+	Screen currentScreen;
+
+	// Passwords
+	List<string> lvl1Passwords = new List<string>() {
+		"password",
+		"home",
+		"123"
+	};
+	List<string> lvl2Passwords = new List<string>() {
+		"pw123",
+		"startupPW",
+		"123!@#"
+	};
+	List<string> lvl3Passwords = new List<string>() {
+		"Goog1!",
+		"remote1!",
+		"1!2@3#"
+	};
+
 
 	// Use this for initialization
 	void Start() {
@@ -19,6 +37,7 @@ public class Hacker : MonoBehaviour {
 	}
 
 	void ShowMainMenu() {
+		currentScreen = Screen.MainMenu;
 		Terminal.ClearScreen();
 		Terminal.WriteLine("Hack locations: ");
 		Terminal.WriteLine("1.  Local");
@@ -28,20 +47,28 @@ public class Hacker : MonoBehaviour {
 	}
 
 	void OnUserInput(string input) {
-		if (input == "menu" || input == "Menu") {
-			currentScreen = Screen.MainMenu;
+		if (input.ToLower() == "menu") {
 			ShowMainMenu();
-		} else if (input == "007") {
-			Terminal.WriteLine("Select a level Mr. Bond.");
-			currentScreen = Screen.MainMenu;
-		} else if (input == "1") {
-			level = 1;
+		} else if (currentScreen == Screen.MainMenu){
+			RunMainMenu(input);
+		} else if (currentScreen == Screen.Password){
+			CheckPassword(input);
+		}
+	}
 
+	void RunMainMenu(string input){
+		if (input == "1") {
+			level = 1;
 			StartGame();
 		} else if (input == "2") {
 			level = 2;
-
 			StartGame();
+		} else if (input == "3") {
+			level = 3;
+			StartGame();
+		} else if (input == "007") {
+			Terminal.WriteLine("Select a level Mr. Bond.");
+			currentScreen = Screen.MainMenu;
 		} else {
 			Terminal.WriteLine("Invalid choice.");
 			currentScreen = Screen.MainMenu;
@@ -50,9 +77,33 @@ public class Hacker : MonoBehaviour {
 
 	void StartGame(){
 		currentScreen = Screen.Password;
-		Terminal.WriteLine("You have chosen level " + level);
+		Terminal.WriteLine("Enter level " + level + " password:");
 	}
 	
+	void CheckPassword(string input) {
+		if (level == 1){
+			if (lvl1Passwords.Contains(input)) {
+				Terminal.WriteLine("I'm in...");
+			}
+			else {
+				Terminal.WriteLine("Try again...");
+			}
+		} else if (level == 2){
+			if (lvl2Passwords.Contains(input)) {
+				Terminal.WriteLine("I'm in...");
+			}
+			else {
+				Terminal.WriteLine("Try again...");
+			}
+		} else if (level == 3){
+			if (lvl3Passwords.Contains(input)) {
+				Terminal.WriteLine("I'm in...");
+			}
+			else {
+				Terminal.WriteLine("Try again...");
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update() {
 		
