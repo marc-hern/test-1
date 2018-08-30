3f2b1d0031836944267c730393be4377ed775cb9
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-08-29 21:59:03 -0700

    Added other level, fix enumeration

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
+	// Game state
+	int level;
+
 	// Use this for initialization
 	void Start() {
 		ShowMainMenu();
@@ -13,25 +16,29 @@ public class Hacker : MonoBehaviour {
 		Terminal.ClearScreen();
 		Terminal.WriteLine("Hack locations: ");
 		Terminal.WriteLine("1.  Local");
-		Terminal.WriteLine("1.  Startup");
-		Terminal.WriteLine("1.  Google");
+		Terminal.WriteLine("2.  Startup");
+		Terminal.WriteLine("3.  Google");
 		Terminal.WriteLine("Enter selection:");
 	}
 
-	void OnUserInput(string input){
+	void OnUserInput(string input) {
 		if (input == "menu" || input == "Menu") {
 			ShowMainMenu();
-		} else if (input == "007"){
+		} else if (input == "007") {
 			Terminal.WriteLine("Select a level Mr. Bond.");
-		} else if (input == "1"){
-			StartGame(1);
-		}else {
+		} else if (input == "1") {
+			level = 1;
+			StartGame();
+		} else if (input == "2") {
+			level = 2;
+			StartGame();
+		} else {
 			Terminal.WriteLine("Invalid choice.");
 		}
 	}
 
-	void StartGame(int inputLevel){
-		Terminal.WriteLine("You have chosen level " + inputLevel);
+	void StartGame(){
+		Terminal.WriteLine("You have chosen level " + level);
 	}
 	
 	// Update is called once per frame
