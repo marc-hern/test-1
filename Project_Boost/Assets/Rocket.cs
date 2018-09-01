41f39892a8e272619b596e8c8873cb54a222a6e7
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-01 09:25:44 -0700

    Added boost audio, fixed audio clipping

--- Project_Boost/Assets/Rocket.cs
+++ Project_Boost/Assets/Rocket.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidBody;
+	AudioSource audioSource;
+	bool soundPlaying = false;
+	float startVolume = 0.101f;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,11 +24,37 @@ public class Rocket : MonoBehaviour {
 	private void ProcessInput() {
 		if (Input.GetKey(KeyCode.Space)) {
 			rigidBody.AddRelativeForce(Vector3.up);
+			if (!soundPlaying){
+				// audioSource.Play();
+				// audioSource.UnPause();
+				soundPlaying = true;
+				audioSource.Play();
+                audioSource.volume = startVolume;
+                audioSource.UnPause();
+			}
+		} else {
+			if (soundPlaying){
+				soundPlaying = false;
+                StartCoroutine(VolumeFade(audioSource, 0f, 0.5f));
+			}
+			// audioSource.Stop();
 		}
 		if (Input.GetKey(KeyCode.A)) {
 			transform.Rotate(Vector3.forward);
 		} else if (Input.GetKey(KeyCode.D)) {
 			transform.Rotate(-Vector3.forward);
 		}
+	}
+
+	IEnumerator VolumeFade(AudioSource _AudioSource, float _EndVolume, float _FadeLength)
+    {
+        float _StartTime = Time.time;
+        while (!soundPlaying &&
+               Time.time < _StartTime + _FadeLength)
+        {
+            _AudioSource.volume = startVolume + ((_EndVolume - startVolume) * ((Time.time - _StartTime) / _FadeLength));
+            yield return null;
+        }
+        if (_EndVolume == 0) { _AudioSource.UnPause(); }
 	}
 }
