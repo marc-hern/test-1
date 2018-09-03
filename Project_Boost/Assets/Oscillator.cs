66f985c1bffad2f3e61ca4212b17542ea0200573
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-02 23:45:02 -0700

    Oscillation of platform

--- Project_Boost/Assets/Oscillator.cs
+++ Project_Boost/Assets/Oscillator.cs
@@ -7,17 +7,19 @@ public class Oscillator : MonoBehaviour {
 
 	[SerializeField]Vector3 movementVector;
 
-	[Range(0,1)]
-	[SerializeField]
-	float movementFactor; // 0 for not moved, 1 for fully moved
+	// 0 for not moved, 1 for fully moved
+	[Range(0,1)][SerializeField]float movementFactor;
+
+	Vector3 startingPos;
 
 	// Use this for initialization
 	void Start () {
-		
+		startingPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 offset = movementFactor * movementVector;
+		transform.position = startingPos + offset;
 	}
 }
